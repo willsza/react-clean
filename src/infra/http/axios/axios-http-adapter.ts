7fae93cb1a4df1b0e9cda7154fd9365a5aa4ab08
wilfrank84 <wilfrank84@gmail.com>
2021-08-08 22:09:21 -0300

    feat: ensure AxiosHttpAdapter returns correct values

--- src/infra/http/axios/axios-http-adapter.ts
+++ src/infra/http/axios/axios-http-adapter.ts
@@ -1,8 +1,12 @@
-import { HttpPostParams } from '@/data/protocols/http/http-post-client'
+import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
 import axios from 'axios'
 
-export class AxiosHttpAdapter {
-  async post (params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body)
+export class AxiosHttpAdapter implements HttpPostClient<any, any> {
+  async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const response = await axios.post(params.url, params.body)
+    return {
+      statusCode: response.status,
+      body: response.data
+    }
   }
 }
