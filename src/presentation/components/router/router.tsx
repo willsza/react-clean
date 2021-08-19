df2f3c7a4502bc122f9cd8177990fc40934f1b6e
wilfrank84 <wilfrank84@gmail.com>
2021-08-18 21:47:47 -0300

    refactor: add global styles

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import '@/presentation/styles/global.scss'
 import { Login } from '@/presentation/pages';
 
 const Router: React.FC = () => {
