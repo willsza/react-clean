e4d7c8b2ce35104f09d847b2d1276b2fb9cc2de1
William Franklin <wilfrank84@gmail.com>
2022-02-18 13:34:21 -0300

    feat: ensure Login start with inputs on required state

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -5,12 +5,12 @@ import { Spinner } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
-  const { isLoading, errorMessage } = useContext(Context)
+  const { state, stateError } = useContext(Context)
 
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
-      { isLoading && <Spinner className={Styles.spinner} /> }
-      { errorMessage && <span className={Styles.error}>{errorMessage}</span> }
+      { state.isLoading && <Spinner className={Styles.spinner} /> }
+      { stateError.main && <span className={Styles.error}>{stateError.main}</span> }
     </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,21 +4,21 @@ import Context from '@/presentation/contexts/form/form-context'
 import Styles from './login-styles.scss'
 import { Footer, FormStatus, LoginHeader, Input } from '@/presentation/components'
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
-
 const Login: React.FC = () => {
-  const [state] = useState<StateProps>({
-    isLoading: false,
-    errorMessage: ''
+  const [state] = useState({
+    isLoading: false
+  })
+
+  const [stateError] = useState({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: ''
   })
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <Context.Provider value={state}>
+      <Context.Provider value={{state, stateError}}>
         <form className={Styles.form}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
